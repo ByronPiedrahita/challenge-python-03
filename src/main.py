c591ffcd5488441f4adedca403be0d1c3120f78f
Byron Piedrahita <byronpiedrahita.programmer@gmail.com>
2020-05-16 23:58:21 -0500

    Texto Secreto Encontrado

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 # Resolve the problem!!
 
+import re
 
 def run():
-    # Start coding here
+    hidden_text=[]
+    with open('encoded.txt', 'r', encoding='utf8') as f:
+        hidden = re.findall(r"[a-z]", f.read())
+        print(hidden)
 
 
 if __name__ == '__main__':
